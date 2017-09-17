35cec55907e6a12592f42385870b419a3e5f09d1
Marco Paredes <marcop_93_04@hotmail.com>
2017-09-17 18:34:29 -0300

    Promise Part 2

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -9,15 +9,26 @@ export class PromotionService {
   constructor() { }
 
   getPromotions(): Promise<Promotion[]>{
-    return Promise.resolve(PROMOTION);
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(PROMOTION), 2000);
+    });
   }
 
   getPromotion(id: number): Promise<Promotion> {
-    return Promise.resolve(PROMOTION.filter((x)=> x.id === id)[0]);
+    // Simulate server latency with 2 second delay
+    return new Promise(resolve => {
+      setTimeout(() => resolve(PROMOTION.filter((x)=> x.id === id)[0]), 2000);
+    });
   }
 
   getFeaturedPromotion(): Promise<Promotion> {
-    return Promise.resolve(PROMOTION.filter((x) => x.featured)[0]);
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => {
+        resolve(PROMOTION.filter((x) => x.featured)[0]);
+      }, 2000);
+    });
   }
 
 }
